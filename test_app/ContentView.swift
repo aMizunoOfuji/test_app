c9fb4e9b59f512f9241617ce4fefbd1f74d712b1
mizuno.ofuji@access-company.com <mizuno.ofuji@access-company.com>
2024-11-28 19:04:32 +0900

    initialized

--- test_app/ContentView.swift
+++ test_app/ContentView.swift
@@ -11,8 +11,8 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+                .imageScale(.small)
+                .foregroundStyle(.red)
             Text("Hello, world!")
         }
         .padding()
